c8f4eb7d29a13013b5ab3b31c65b07e43d9394a0
thankuharitha54-gif <thankuharitha54@gmail.com>
2026-05-09 21:32:50 +0530

    css correction

--- src/pages/UserNav.jsx
+++ src/pages/UserNav.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
-import './userNav.css'
+import './usernav.css'
 
 function UserNav() {
     const navigate = useNavigate()
